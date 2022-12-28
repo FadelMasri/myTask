57de48ab2560a4505f8c8a67b592ca046d020a0f
FadelMasri <masrifadel0@gmail.com>
2022-12-28 16:21:02 +0200

    Task

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if(_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already Exist. ");
+            }
+            CreatePasswordHash(request.Password,
+                out byte[] passwordHash,
+                out byte[] passwordSalt );
+        }
+        Private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
+        { 
+        
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password))
+            }
+        }
+
+    }
+}
---
+++ Models/UserRegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Passwordjj")]
+        public string ConfirmPassword { get; set; }
+
+    }
+
+  
+}
